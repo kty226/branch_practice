6c2d6a88cc338b21ba30640ef93250235e783149
{kty226} <{ktyoo226@naver.com}>
2022-05-06 15:25:17 +0900

    feat: re-write

--- three.py
+++ three.py
@@ -1,6 +1,9 @@
-for i in range(1, 20+1):
-	if i%3==0:
-		print("year")
-	else:
-		print(i)
-
+for i in range(1,25+1):
+    if i%15==0:
+        print('yeardream')
+    elif i%3==0:
+        print('year')
+    elif i%5==0:
+        print('dream')
+    else:
+        print(i)
